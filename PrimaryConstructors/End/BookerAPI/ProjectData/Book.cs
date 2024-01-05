f989116894f5cecdfaa5d6ece06f478cf604d989
Holmand Villalba <holmand.villalba@evertecinc.com>
2024-01-05 14:11:21 -0500

    Initial commit
    Add primary constructors solution example

---
+++ PrimaryConstructors/End/BookerAPI/ProjectData/Book.cs
@@ -0,0 +1,13 @@
+﻿namespace BookerAPI.ProjectData
+{
+    public class Book(string title, string author, int year, int id)
+    {
+        public Book() : this("Blood and fire", "George Martin", 2010, 4)
+        { }
+
+        public int Id => id;
+        public string Title => title;
+        public string Author => author;
+        public int Year => year;
+    }
+}
